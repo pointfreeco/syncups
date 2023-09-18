f2d38a417ec91a544b4f06fb9065e2770cd7dc17
Brandon Williams <mbrandonw@hey.com>
2023-09-18 15:05:32 -0400

    More integration tests.

--- Standups-StackBased/StandupsUITests/StandupsListUITests.swift
+++ Standups-StackBased/StandupsUITests/StandupsListUITests.swift
@@ -7,14 +7,15 @@ import XCTest
 // The key to doing this is to set a launch environment variable on your XCUIApplication instance,
 // and then check for that value in the entry point of the application. If the environment value
 // exists, you can use 'withDependencies' to override dependencies to be used in the UI test.
+@MainActor
 final class StandupsListUITests: XCTestCase {
   var app: XCUIApplication!
 
   override func setUpWithError() throws {
     self.continueAfterFailure = false
     self.app = XCUIApplication()
-    app.launchEnvironment = [
-      "UITesting": "true"
+    self.app.launchEnvironment = [
+      "SWIFT_DEPENDENCIES_CONTEXT": "preview"
     ]
   }
 
@@ -28,10 +29,11 @@ final class StandupsListUITests: XCTestCase {
   // the standup is added to the list its data will be persisted to disk so that it will be
   // available on next launch.
   func testAdd() throws {
-    app.launch()
-    app.navigationBars["Daily Standups"].buttons["Add"].tap()
+    self.app.launchEnvironment["UITest"] = String(#function.dropLast(2))
+    self.app.launch()
 
-    let collectionViews = app.collectionViews
+    self.app.navigationBars["Daily Standups"].buttons["Add"].tap()
+    let collectionViews = self.app.collectionViews
     let titleTextField = collectionViews.textFields["Title"]
     let nameTextField = collectionViews.textFields["Name"]
 
@@ -41,10 +43,46 @@ final class StandupsListUITests: XCTestCase {
     nameTextField.typeText("Blob")
 
     collectionViews.buttons["New attendee"].tap()
-    app.typeText("Blob Jr.")
+    self.app.typeText("Blob Jr.")
 
-    app.navigationBars["New standup"].buttons["Add"].tap()
+    self.app.navigationBars["New standup"].buttons["Add"].tap()
 
     XCTAssertEqual(collectionViews.staticTexts["Engineering"].exists, true)
+  }
+
+  func testDelete() async throws {
+    self.app.launchEnvironment["UITest"] = String(#function.dropLast(2))
+    self.app.launch()
+
+    XCTAssertEqual(self.app.staticTexts["Design"].exists, true)
+    self.app.cells.firstMatch.tap()
+
+    self.app.buttons["Delete"].tap()
+    XCTAssertEqual(self.app.staticTexts["Delete?"].exists, true)
+
+    self.app.buttons["Yes"].tap()
+    try await Task.sleep(for: .seconds(0.3))
+    XCTAssertEqual(self.app.staticTexts["Design"].exists, false)
+    XCTAssertEqual(self.app.staticTexts["Daily Standups"].exists, true)
+  }
+
+  func testEdit() async throws {
+    self.app.launchEnvironment["UITest"] = String(#function.dropLast(2))
+    self.app.launch()
+
+    XCTAssertEqual(self.app.staticTexts["Design"].exists, true)
+    self.app.cells.firstMatch.tap()
+
+    self.app.buttons["Edit"].tap()
+    let titleTextField = self.app.textFields["Title"]
+    titleTextField.typeText(" & Product")
+
+    self.app.buttons["Done"].tap()
+    XCTAssertEqual(self.app.staticTexts["Design & Product"].exists, true)
+
+    self.app.buttons["Daily Standups"].tap()
+    try await Task.sleep(for: .seconds(0.3))
+    XCTAssertEqual(self.app.staticTexts["Design & Product"].exists, true)
+    XCTAssertEqual(self.app.staticTexts["Daily Standups"].exists, true)
   }
 }
--- Standups-TreeBased/Standups/StandupsApp.swift
+++ Standups-TreeBased/Standups/StandupsApp.swift
@@ -9,8 +9,8 @@ struct StandupsApp: App {
       //     dependencies for the duration of the test (e.g. the data manager). We do not really
       //     recommend performing UI tests in general, but we do want to demonstrate how it can be
       //     done.
-      if ProcessInfo.processInfo.environment["UITesting"] == "true" {
-        UITestingView()
+      if let testName = ProcessInfo.processInfo.environment["UITest"] {
+        UITestingView(testName: testName)
       } else {
         StandupsList(model: StandupsListModel())
       }
@@ -19,9 +19,18 @@ struct StandupsApp: App {
 }
 
 struct UITestingView: View {
+  let testName: String
+
   var body: some View {
     withDependencies {
-      $0.dataManager = .mock()
+      switch testName {
+      case "testAdd":
+        $0.dataManager = .mock()
+      case "testDelete", "testEdit":
+        $0.dataManager = .mock(initialData: try? JSONEncoder().encode([Standup.mock]))
+      default:
+        fatalError()
+      }
     } operation: {
       StandupsList(model: StandupsListModel())
     }
